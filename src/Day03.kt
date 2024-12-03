d5a4f3cad67f2dea3a19abec60cab7cd1c340f0e
Kaloyan Karaivanov <skarloti@gmail.com>
2024-12-03 14:01:29 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/Day03.kt

--- src/Day03.kt
+++ src/Day03.kt
@@ -3,21 +3,19 @@ fun main() {
     val instructionRegex = Regex("""mul\((\d{1,3}),(\d{1,3})\)""")
     val dontComputeRegex = Regex("""(don't\(\).*?do\(\))|$""")
 
-    fun load(fileName: String) { lines = readInput(fileName) }
-
-    fun String.compute() = instructionRegex.findAll(this).sumOf {
-        val (a, b) = it.destructured
+    fun String.compute(): Int = instructionRegex.findAll(this).sumOf { match ->
+        val (a, b) = match.destructured
         (a.toInt() * b.toInt())
     }
 
     fun part1(): Int = lines.sumOf(String::compute)
     fun part2(): Int = lines.joinToString("").split(dontComputeRegex).sumOf(String::compute)
 
-    load("Day03_test")
+    lines = readInput("Day03_test")
     check(part1() == 161) { "incorrect result part1" }
     check(part2() == 48) { "incorrect result part2" }
 
-    load("Day03")
+    lines = readInput("Day03")
     part1().println()
     part2().println()
 }
